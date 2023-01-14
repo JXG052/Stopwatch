db9b2ce7c6de5fe3f16af6ade177e49c9c89cb89
JXG052 <jxg0521@gmail.com>
2023-01-14 18:48:02 +0000

    basic CSS but logic working

--- my-attempt/script.js
+++ my-attempt/script.js
@@ -2,6 +2,7 @@
 const startBtn = document.querySelector(".btn");
 const pauseBtn = document.getElementById("pauseBtn");
 const counter = document.querySelector(".counter");
+const restartBtn = document.querySelector("#restartBtn");
 let count = 0;
 // const intId = setInterval(countFunction, 1000);
 let intervalId
@@ -16,4 +17,11 @@ startBtn.addEventListener("click", function (){
 pauseBtn.addEventListener("click", function() {
     clearInterval(intervalId)
 })
+
+restartBtn.addEventListener("click", function(){
+    clearInterval(intervalId)
+    count = 0;
+    counter.textContent = count;
+})
+
 
